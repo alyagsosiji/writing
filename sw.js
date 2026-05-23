54078657a5117962cd8c0ced6e8c2a02268e234c
알약소시지 <alyagsosiji@gmail.com>
2026-05-23 15:37:32 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+// sw.js - 모바일 백그라운드 알림 활성화용 서비스 워커
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+});
 
+self.addEventListener('activate', function(event) {
+    event.waitUntil(self.clients.claim());
+});
+
+// 알림 클릭 시 앱 창으로 포커스 이동
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+            if (clientList.length > 0) {
+                return clientList[0].focus();
+            }
+            return clients.openWindow('/');
+        })
+    );
+});
